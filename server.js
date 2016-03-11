1c376202a4309a92980caddc1fbac23ec687d5fb
himanshu-hearthacker <himkiet@gmail.com>
2016-03-11 19:30:06 +0530

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -20,13 +21,13 @@ res.json(todos); //this converted todos into json
 })
 app.get('/todos/:id', function(req, res){
 var todoId = parseInt(req.params.id,10);
-console.log(typeof todoId);
-var matchedTodo;
-todos.forEach(function (todo){
-	if (todoId === todo.id) {
-		matchedTodo = todo;
-	}
-});
+//console.log(typeof todoId);
+var matchedTodo = _.findWhere(todos, {id: todoId});
+//todos.forEach(function (todo){
+	//if (todoId === todo.id) {
+//		matchedTodo = todo;
+//	}
+//});
 if (matchedTodo) {
 	res.json(matchedTodo);
 }
@@ -41,11 +42,25 @@ else {
 app.post('/todos', function (req, res){
  var body = req.body;
 
- body.id = todoNextId++;
- todos.push(body);
+ // use pick method  to only pick description and completed
+
+ var picked = _.pick(body,'completed','description');
+
+ if ((!_.isBoolean(picked.completed)) || (!_.isString(picked.description)) || (picked.description.trim().length === 0))
+  {
+
+ 	return res.status(400).send();
+ }
+
+picked.description ===picked.description.trim();
+
+
+
+ picked.id = todoNextId++;
+ todos.push(picked);
 
  console.log (todos);
- res.json(body);
+ res.json(picked);
 
 }) ;
 
